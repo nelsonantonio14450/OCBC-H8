9f7fb6a0d963e747c9364169607d37d228c55b5a
Nelson Antonio <nelson.antonio14450@gmail.com>
2021-10-25 11:45:01 +0700

    assign4

---
+++ assignment4/kalkulator.js
@@ -0,0 +1,11 @@
+let a = 10;
+let b = 2;
+
+console.log("a tambah b : ", a + b);
+console.log("a minus b : ", a - b);
+console.log("a times b : ", a * b);
+console.log("a divide b : ", a / b);
+console.log("a Mod b : ", a % b);
+console.log("a plus 1 : ", ++a);
+console.log("b minus 1 : ", --b);
+
---
+++ sesi17/index.js
@@ -0,0 +1,15 @@
+var greeting = "selamat datang";
+let name = "yosafat";
+const pi = 3.14;
+
+// console.log(greeting, name);
+
+let nilai = 50;
+
+if (nilai >= 80) {
+    console.log("A");
+} else if (nilai >= 70) {
+    console.log("B");
+} else {
+    console.log("C");
+}
